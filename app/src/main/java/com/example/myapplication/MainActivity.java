8b080bcec29a9ca15876b60fe2db29e76f30f05e
slava <englisheniks@gmail.com>
2021-08-18 21:30:37 +0300

    compact

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,25 +20,20 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     int t1h, t1m, t2h, t2m;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         T1  = (TextView) findViewById(R.id.T1);
         sr  = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
         T2  = (TextView) findViewById(R.id.T2);
 
-
 // first clock
         T1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 TimePickerDialog timePickerDialog = new TimePickerDialog(  
                         MainActivity.this,
                         new TimePickerDialog.OnTimeSetListener() {
@@ -54,10 +49,8 @@ public class MainActivity extends AppCompatActivity {
                 );
                 timePickerDialog.updateTime(t1h,t1m);
                 timePickerDialog.show();
-
             }
         });
-
 // second clock
         T2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,17 +65,13 @@ public class MainActivity extends AppCompatActivity {
                                 Calendar calendar = Calendar.getInstance();
                                 calendar.set(0, 0, 0, t2h, t2m);
                                 T2.setText(android.text.format.DateFormat.format( "hh-mm a",calendar));
-
                             }
                         }, 12, 0, false
                 );
                 timePickerDialog.updateTime(t2h,t2m);
                 timePickerDialog.show();
-
             }
         });
-
-
 
 // try to count but i can't
         button.setOnClickListener(new View.OnClickListener() {
@@ -90,25 +79,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 SimpleDateFormat sdf = new SimpleDateFormat("hh-mm");
 
-
                 double c,x,y;
-
 
                 String S3 = T1.getText().toString();
                 String S4 = T2.getText().toString();
 
 
-
                 x = Double.parseDouble(S3);
                 y = Double.parseDouble(S4);
 
-
                 c = y - x;
 
-
-
                 String S = Double.toString(c);
-
 
                 sr.setText(S);
             }
